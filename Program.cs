e405d5b9e0f1244125edab19e1c73ccdd319e844
Anton Pashkun <a.pashkun@godeltech.com>
2021-02-08 14:32:41 +0300

    Using Boxing Unboxing cases

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace BoxingUnboxingLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пример упаковки (Boxing):
+
+            int i = 123;      // Инициализируем переменную "i";
+            object obj1 = i; // (Boxing)- копируем значение i в значение object obj;
+
+            // Пример распаковки (Unboxing):
+
+            //int i = 123;        // "i" здесь является значением типа;
+            //object obj1 = i;   //  упаковка (Boxing): "i" в "obj1";
+            int obj2 = (int)obj1; // распаковка (Unboxing): " obj1" в "obj2".
+
+        }
+    }
+}
